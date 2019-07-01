62a3bd5a10d6fb245f552df1dcc52fc7fba2493f
Pozharitskiy <33429207+Pozharitskiy@users.noreply.github.com>
2019-07-01 17:42:20 +0300

    Add elegant solution for 10th task

--- 27.06/tenth.js
+++ 27.06/tenth.js
@@ -1,3 +1,21 @@
 function isEqual(ob1, ob2) {
-   /*Of course :)*/return JSON.stringify(ob1) === JSON.stringify(ob2);
+   /*Of course :)*/
+   const string1 = JSON.stringify(ob1);
+   const string2 = JSON.stringify(ob2);
+   const twoStrings = (string1 + string2).replace(/[.,\/#!$%\^&\'"\*;:{}=\-_`~()]/g, "");
+   const mas = twoStrings.split('');
+   console.log(twoStrings);
+   let test = mas.reduce((acc, el) => {
+      acc[el] = (acc[el] || 0) + 1;
+      return acc;
+   }, {});
+
+   console.log(test);
+   var keys = Object.keys(test);
+   var dupe = false;
+   const values = Object.values(test);
+   const isTwo = (el) => {
+      return (el === 2);
+   }
+   (values.every(isTwo) ? console.log(true) : console.log(false))
 }
